32f7b6ad26750abf6333a0564064217a751a5112
신혜리 <ccori12@naver.com>
2021-11-21 23:01:15 +0900

    project initial setting

---
+++ src/routes/index.js
@@ -0,0 +1,13 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+const RootRoute = () => {
+    return(
+        <Router>
+            <Routes>
+                <Route />
+            </Routes>
+        </Router>
+    )
+}
+
+export default RootRoute
